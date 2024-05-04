c5e044b4727d548a4e46f9993b73bea6a2cec976
Guocork <g1024536444@gmail.com>
2024-05-04 22:56:15 +0800

    创建client部分内容

---
+++ client/src/main.rs
@@ -0,0 +1,16 @@
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("failed to initiate non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();
+}
