74b41f4ac29b6116233bec6e0ce32a81a5345a25
Guocork <g1024536444@gmail.com>
2024-05-06 21:48:03 +0800

    刨析client代码 搞懂缓冲区、字节传输、主以及工作线程

--- client/src/main.rs
+++ client/src/main.rs
@@ -35,12 +35,19 @@ fn main() {
 
     let (tx, rx) = mpsc::channel::<String>();
 
-    thread::spawn(move || loop {
-        let mut buff = vec![0;MSG_SIZE];
+    thread::spawn(move || loop {  // 这里只创建了一个工作线程 这利使用了move关键字 可以将外面定义的管道的所有权给工作线程
+        /*
+         * 这里设置缓冲区的目的：
+         * 1. 直接操作内存缓冲区，比频繁操作io更加高效，我的理解是，当TCP数据报回来时候，直接将其中的数读到缓冲区利，方便操作，不用一直动不动就去其他网络层调用io
+         * 2. 可以提前预留好符合网络标准的数据空间，不用每次在进行读取改写
+         * 3. 在非阻塞I/O模型中，如果没有足够的数据立即可用，读取操作可能会返回错误（如WouldBlock）。使用缓冲区可以在数据逐步累积到足够处理时再进行处理，而不是立即报告错误，这提供了更细粒度的控制和错误处理策略。
+         */
+        let mut buff = vec![0;MSG_SIZE];  // 创建了一个包含32长度的数组 每个值都是0 将每个值设置为0的目的是：读入数据时，剩余未使用的缓冲区部分保持为零值
 
-        match client.read_exact(&mut buff) {   // 这段代码 是工作线程用来处理 从服务器读取到的消息
+        match client.read_exact(&mut buff) {  // 这段代码 是工作线程用来从TCP 连接中读取数据 这个exact方法是正好可以读取到符合32限制的数据
+
             Ok(_) => {
-                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();  // 这个msg表示接受到了 有效的新数组信息
                 println!("message recv {:?}",msg);
             },
             Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
@@ -65,11 +72,15 @@ fn main() {
     });
 
     println!("write a Message:");
+
     loop {
-        let mut buff = String::new();
-        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
-        let msg = buff.trim().to_string();
-        if msg == ":quit" || tx.send(msg).is_err() {
+        let mut buff = String::new();  // 创建一个缓冲区
+
+        io::stdin().read_line(&mut buff).expect("reading from stdin failed");  // 这里是主线程获得用户的输入，并且放入缓冲区
+
+        let msg = buff.trim().to_string(); 
+
+        if msg == ":quit" || tx.send(msg).is_err() {  // 只要如果接收端已经关闭，无法再接收更多的消息。返回err 就break
             break;
         }
     }
